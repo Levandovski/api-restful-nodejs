4b6c23e15b8c55714d49db5c189f559210207d68
JesJessé Levandovski <jesse.levandovski@outlook.com>
2022-04-25 22:26:06 -0300

    New implemenatiton of middleware the rest

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,12 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import routes from '@shared/routes';
-import { request } from 'http';
-import AppError from '@shared/errors/AppError';
-import '@shared/typeorm';
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { errors } from "celebrate";
+import "express-async-errors";
+import routes from "@shared/routes";
+import { request } from "http";
+import AppError from "@shared/errors/AppError";
+import "@shared/typeorm";
 
 const app = express();
 
@@ -13,22 +15,24 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(errors());
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
-        status: 'error',
+        status: "error",
         messsage: error.message,
       });
     }
 
-    return response.status(500).json(({
-      status: 'error',
-      message: 'Internal server error',
-    }))
-  },
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
 );
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log("Server started on port 3333!");
 });
